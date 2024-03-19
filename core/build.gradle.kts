ff72ae66aef4de3cc381ea62a3d362e38a10460b
Hilda003 <Hildatulwardah003@gmail.com>
2024-03-19 22:43:01 +0700

    Add layout DetailActivity, fragment_follow, fragment_favorite, and activity_splash_screen

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
